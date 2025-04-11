b78ec8dd11403e7438756f57d4ec7b68d4beda64
rahulinfolabz <rahulinfolabz@gmail.com>
2025-04-11 12:31:14 +0530

    Mobile Api Changed

--- MOBILE API/AddMobileInq.js
+++ MOBILE API/AddMobileInq.js
@@ -22,7 +22,7 @@ async function AddMobileAccessoryInquiry(req, res) {
 
     return res
       .status(200)
-      .json({ sucess: true, message: "Accessory Inquiry Added !" });
+      .json({ success: true, message: "Accessory Inquiry Added !" });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ success: false, message: error.message });
